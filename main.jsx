4f4e083dc8772351344d7ff1f7979d16af48f540
Haris Yousaf <154906048+haris-yousaf@users.noreply.github.com>
2024-08-01 22:00:26 +0500

    main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Practice2 from './Practice2.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Practice2 />
+  </React.StrictMode>,
+)
